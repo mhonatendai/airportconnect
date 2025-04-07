9ab15419bf733111aa55599152024e0b1040cc71
tendai <mhonatendai@gmail.com>
2025-04-07 14:56:45 -0400

    first commit

---
+++ src/main/java/com/ten/airportconnect/AirportconnectApplication.java
@@ -0,0 +1,13 @@
+package com.ten.airportconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ten/airportconnect/AirportconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ten.airportconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
